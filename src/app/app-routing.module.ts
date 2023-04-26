f31b70ab79922f8c1eceac0f67c133e1e89ca7f9
Harel Natan <harelnatan7@gmail.com>
2023-04-26 03:06:13 +0300

    added base elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TravelingFormComponent } from './traveling-form/traveling-form.component';
+import { TravelingTableComponent } from './traveling-table/traveling-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TravelingFormComponent,
+  },
+  {
+    path: 'form',
+    component: TravelingFormComponent,
+  },
+  {
+    path: 'table',
+    component: TravelingTableComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelingFormComponent } from './traveling-form/traveling-form.component';
+import { TravelingTableComponent } from './traveling-table/traveling-table.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelingFormComponent,
+    TravelingTableComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
